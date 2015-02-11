6f5980afa84861a37852b3e394968f1d57825140
FlyC <yayayahahaha0011@yahoo.com.tw>
2015-02-12 03:52:27 +0800

    rails generate controller Categories index show new edit delete create a new controller include five method of CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'categories/index'
+
+  get 'categories/show'
+
+  get 'categories/new'
+
+  get 'categories/edit'
+
+  get 'categories/delete'
+
 	get 'pages/home'
 	root 'pages#home'
 
