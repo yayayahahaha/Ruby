438f3290504edcccd9ed9ea2b455b3f957f1f9a2
FlyC <yayayahahaha0011@yahoo.com.tw>
2015-02-12 03:20:18 +0800

    add erb_demo file, update routes.rb and pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
 	def home
 	end
+
+	def erb_demo
+	end
+	
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
 	get 'pages/home'
 	root 'pages#home'
+
+	get 'pages/erb_demo'
 end
