76a3389b92c06a02dc60edd72408a29c6c2c81a8
FlyC <yayayahahaha0011@yahoo.com.tw>
2015-02-23 20:27:55 +0800

    create a new controller: "Products"
    include five method:
    index show new edit delete
    type "rails generate controller products index show new edit delete"

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'products/index'
+
+  get 'products/show'
+
+  get 'products/new'
+
+  get 'products/edit'
+
+  get 'products/delete'
+
 =begin
   get 'categories/index'
 
