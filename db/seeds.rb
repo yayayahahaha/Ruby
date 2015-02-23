7d9bee18cd01d94c405ef1090659a0e22deed863
FlyC <yayayahahaha0011@yahoo.com.tw>
2015-02-23 20:18:42 +0800

    re-seed database
    reseed database:
    Product.create(...., category_id: ruby.id)

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,8 @@ ruby = Category.create name: 'Ruby', url: 'Red'
 weiss = Category.create name: 'Weiss', url: 'White'
 blake = Category.create name: 'Blake', url: 'Black'
 yang = Category.create name: 'Yang', url: 'Yellow'
+
+Product.create(:product_name => "Sickle", :description => "color_red", :price => 1, :url => "first", category_id: ruby.id)
+Product.create(:product_name => "Fencing", :description => "color_white", price: 2, url: "second", category_id: weiss.id)
+Product.create(:product_name => "Kinife", :description => "color_black", price: 3, url: "third", category_id: blake.id)
+Product.create(:product_name => "Fist", :description => "color_yellow", price: 4, url: "fourth", category_id: yang.id)
